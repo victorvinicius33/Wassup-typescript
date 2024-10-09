c5fdcd10b0eb037bc72671c51a9a5fbb1e36464e
Victor Dell <victorvinicius33.vv@gmail.com>
2024-10-09 09:41:18 -0300

    feat: add socket.io config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,56 @@
-import express from 'express';
+require('dotenv').config()
+import express from 'express'
+const app = express()
+import { Server } from 'socket.io'
+import cors from 'cors'
+import routes from './routes'
 
-const app = express();
+const server = require('http').createServer(app)
 
-app.use(express.json());
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST'],
+  },
+})
 
-app.listen('3000', () => {
-  console.log('Server is running on port 3000');
-});
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(express.json())
+app.use(routes)
+
+const users: any = {}
+
+io.on('connection', (socket) => {
+  socket.on('new_user', (data) => {
+    users[data.userId] = socket
+  })
+
+  socket.on('join_room', (data) => {
+    socket.join(data)
+  })
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data)
+  })
+
+  socket.on('disconnect', () => {
+    delete users[socket.id]
+
+    io.emit('user_disconnected')
+  })
+
+  socket.on('add_contact', (data) => {
+    socket.join(data.roomId)
+    users[data.contactId] && users[data.contactId].join(data.roomId)
+
+    socket.to(data.roomId).emit('new_contact')
+
+    socket.leave(data.roomId)
+    users[data.contactId] && users[data.contactId].leave(data.roomId)
+  })
+})
+
+server.listen(process.env.PORT, () => {
+  console.log('Server running on port 3000')
+})
