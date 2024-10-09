35e4a33bc96ec03490b3ff4f802b4b7016b90586
Victor Dell <victorvinicius33.vv@gmail.com>
2024-10-09 09:40:52 -0300

    feat: add signup route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,19 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+import { CreateUserController } from './controller/create-user/create-user'
+import { PgCreateUserRepository } from './repositories/create-user/pg-create-user'
 
 const routes = Router()
+
+routes.post('/users', async (req: Request, res: Response) => {
+  const pgCreateUserRepository = new PgCreateUserRepository()
+
+  const createUserController = new CreateUserController(pgCreateUserRepository)
+
+  const { body, statusCode } = await createUserController.handle({
+    body: req.body,
+  })
+
+  res.status(statusCode).json(body)
+})
 
 export default routes
