0c53decb0b5c5ce575e045abe271404f7db201e3
Victor Dell <victorvinicius33.vv@gmail.com>
2024-10-09 09:39:09 -0300

    feat: add PgCreateUserRepository

---
+++ src/repositories/create-user/pg-create-user.ts
@@ -0,0 +1,26 @@
+import {
+  CreateUserParams,
+  ICreateUserRepository,
+} from '../../controller/create-user/protocols'
+import { IUser } from '../../models/user'
+import { knex } from '../../database/connection'
+import bcrypt from 'bcrypt'
+
+export class PgCreateUserRepository implements ICreateUserRepository {
+  async createUser(params: CreateUserParams): Promise<IUser> {
+    const { name, email, password } = params
+
+    const hashPassword = await bcrypt.hash(password, 10)
+    const newUser = await knex('users')
+      .insert({ name, email, password: hashPassword })
+      .returning('*')
+
+    if (!newUser) {
+      throw new Error('User not created.')
+    }
+
+    const { password: _, ...newUserData } = newUser[0]
+
+    return newUserData
+  }
+}
