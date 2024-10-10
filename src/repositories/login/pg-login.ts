542ba4be3284d7419b7a147ef59cabaceba0746e
Victor Dell <victorvinicius33.vv@gmail.com>
2024-10-10 05:24:19 -0300

    feat: add LoginRepository

---
+++ src/repositories/login/pg-login.ts
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken'
+import { ILoggedUserData } from '../../models/user'
+import { knex } from '../../database/connection'
+import bcrypt from 'bcrypt'
+import { ILoginRepository, loginParams } from '../../controller/login/protocols'
+
+export class PgLoginRepository implements ILoginRepository {
+  async login(params: loginParams): Promise<ILoggedUserData> {
+    const { email, password } = params
+
+    console.log('to aqui')
+
+    const user = await knex('users').where('email', email).first()
+    if (!user) {
+      throw new Error('Incorrect E-mail or password.')
+    }
+
+    const correctPassword = await bcrypt.compare(password, user.password)
+    if (!correctPassword) {
+      throw new Error('Incorrect E-mail or password')
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET ?? '', {
+      expiresIn: '8h',
+    })
+
+    const allContacts = await knex('contacts')
+      .where({ user_id: user.id })
+      .returning('*')
+
+    const { password: _, ...userData } = user
+
+    return {
+      user: userData,
+      contacts: allContacts,
+      token,
+    }
+  }
+}
