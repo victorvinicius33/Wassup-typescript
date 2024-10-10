fc392d05551f0ae0108065fe55863745fcf29e52
Victor Dell <victorvinicius33.vv@gmail.com>
2024-10-10 05:25:03 -0300

    feat: add login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express'
 import { CreateUserController } from './controller/create-user/create-user'
 import { PgCreateUserRepository } from './repositories/create-user/pg-create-user'
+import { PgLoginRepository } from './repositories/login/pg-login'
+import { LoginController } from './controller/login/login'
 
 const routes = Router()
 
@@ -10,6 +12,18 @@ routes.post('/users', async (req: Request, res: Response) => {
   const createUserController = new CreateUserController(pgCreateUserRepository)
 
   const { body, statusCode } = await createUserController.handle({
+    body: req.body,
+  })
+
+  res.status(statusCode).json(body)
+})
+
+routes.post('/login', async (req: Request, res: Response) => {
+  const pgLoginRepository = new PgLoginRepository()
+
+  const loginController = new LoginController(pgLoginRepository)
+
+  const { body, statusCode } = await loginController.handle({
     body: req.body,
   })
 
